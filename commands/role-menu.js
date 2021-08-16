bf930751fed87ec7130dc499b86a357a8c10f7c8
Daniel  Breton <8995597+DanBreton@users.noreply.github.com>
2021-08-16 13:08:53 +0100

    Improved role menu builder

--- commands/role-menu.js
+++ commands/role-menu.js
@@ -15,13 +15,17 @@ function buildSlashCommand()
 
 	for (let i = 0; i < roleMenus.length; i++) {
 		let roleMenu = roleMenus[i];
-		console.log(roleMenu.name);
 		stringOption.addChoice(roleMenu.name, roleMenu.name);
 	}
 
 	command.addStringOption(stringOption);
 
 	return command;
+}
+
+function removeEmojis(string)
+{
+	return string.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
 }
 
 module.exports = {
@@ -37,18 +41,27 @@ module.exports = {
 			if(roleMenu.name == interaction.options.getString('menu'))
 			{
 				embed.setTitle(roleMenu.title);
-				embed.setDescription(roleMenu.description);
+
+				let description = roleMenu.description + "\n\n";
 
 				for(let j = 0; j < roleMenu.buttons.length; j++)
 				{
 					let button = roleMenu.buttons[j];
+					const role = interaction.guild.roles.cache.get(button.roleId);
+
 					const msgBtn = new MessageButton()
 					.setStyle(button.style)
 					.setEmoji(button.emoji)
-					.setLabel(button.label)
+					.setLabel(removeEmojis(role.name))
 					.setCustomId(button.customId);
 					row.addComponents(msgBtn);
+
+					description += `${role}` + "\n";
 				}
+
+				embed.setDescription(description);
+				embed.setColor(roleMenu.colour);
+
 				menuFound = true;
 				break;
 			}
